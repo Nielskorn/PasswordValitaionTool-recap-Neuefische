4f1067a560f7735b9826a86df9babdebb7e37c18
Nielskorn <38701871+Nielskorn@users.noreply.github.com>
2024-11-25 11:01:31 +0100

    implement PasswordValidation validatePasswordLength

--- src/main/java/org/neuefische/PasswordValidation.java
+++ src/main/java/org/neuefische/PasswordValidation.java
@@ -2,5 +2,6 @@ package org.neuefische;
 
 public class PasswordValidation {
     public static boolean validatePasswordLength(String givenPassword) {
+       return givenPassword.length()>7;
     }
 }
