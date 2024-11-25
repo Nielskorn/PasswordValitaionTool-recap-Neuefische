37496f8688f054302d002e361ac4b3b6cad6f088
Nielskorn <38701871+Nielskorn@users.noreply.github.com>
2024-11-25 09:53:54 +0100

    inti commit gitignore add .idea folder to ignore list add first Testcases(wip)

---
+++ src/test/java/passwordValidationTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+public class passwordValidationTest {
+
+    @Test
+   void passwordlength_check(){
+
+    }
+    @Test
+    void passwordCaseCheck(){
+    ;
+    }
+}
