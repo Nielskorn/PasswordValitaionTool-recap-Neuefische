73420b812def8e8279e11e53563deb7270200526
Nielskorn <38701871+Nielskorn@users.noreply.github.com>
2024-11-25 11:53:33 +0100

    implement methode for ValidationThatGivenPasswordIsNotCommon

--- src/main/java/org/neuefische/PasswordValidation.java
+++ src/main/java/org/neuefische/PasswordValidation.java
@@ -1,6 +1,23 @@
 package org.neuefische;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class PasswordValidation {
+    static File commonPasswords= new File(
+
+            "src/main/resources/commonPasswords.txt");
+    static Scanner scanner;
+
+    static {
+        try {
+            scanner = new Scanner(commonPasswords);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public static boolean validatePasswordLength(String givenPassword) {
        return givenPassword.length()>7;
     }
@@ -15,4 +32,22 @@ public class PasswordValidation {
           return false;
       } else return !passwordUpperCase.equals(givenPassword);
       }
+
+
+    public static boolean validatePasswordNotCommon(String givenPassword) {
+    givenPassword = givenPassword.toLowerCase();
+        try{
+
+        while(scanner.hasNextLine()){
+            String line=scanner.nextLine();
+            if(line.contains(givenPassword)){
+                return false;
+            }
+        }
+        return true;
+    }catch (Exception e){
+        e.printStackTrace();
+    }
+    return false;
+    }
 }
